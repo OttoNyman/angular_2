2e7a30fd556e7de18d8b68cbca01d2e5cc790e5d
OttoNyman <7716116@gmail.com>
2021-07-13 16:19:16 +0300

    http lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core'
-import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '@angular/forms'
+
+export interface Todo {
+  title: string;
+  completed: boolean;
+  userId?: number
+}
 
 @Component({
   selector: 'app-root',
@@ -7,47 +13,16 @@ import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  form!: FormGroup
+  mytodos: Todo[] = [];
+
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit() {
-    this.form = new FormGroup({
-      email: new FormControl('', [
-        Validators.email,
-        Validators.required
-      ]),
-      password: new FormControl('', [
-        Validators.required,
-        Validators.minLength(6)
-      ]),
-      address: new FormGroup({
-        country: new FormControl('ua'),
-        city: new FormControl('', [Validators.required]),
-      }),
-      skills:new FormArray([])
-    })
-  }
-
-  submit() {
-    if (this.form.valid) {
-      console.log('Form: ', this.form)
-      console.log('Form value:', this.form.value)
-    }
-  }
-
-  setCapital() {
-    const cityMap: { [code: string]: string } = {
-      ru: 'Москва',
-      ua: 'Киев',
-      by: 'Минск'
-    }
-    const cityKey = this.form.controls.address.value.country
-    const city = cityMap[cityKey]
-    this.form.patchValue({ address: { city } })
-  }
-  addSkill(){
-    const skillControl=new FormControl('', Validators.required);
-    (<FormArray>this.form.get('skills')).push(skillControl)
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=3')
+      .subscribe(gettedtodos => {
+        console.log('Ответ:', gettedtodos)
+        this.mytodos = gettedtodos
+      })
   }
 }
-
-AbstractControl
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -9,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent,
   ],
   imports: [
-    BrowserModule, FormsModule, ReactiveFormsModule
+    BrowserModule,
+    FormsModule, 
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
